29f4b64413ccda9621897a1b03c16f85818540da
minbr0ther <minbr0ther@naver.com>
2022-02-15 13:40:36 +0900

    feat: section01 구현

--- components/Section01/index.tsx
+++ components/Section01/index.tsx
@@ -1,7 +1,31 @@
 import * as S from "./styled";
+import useScrollFadeIn from "hooks/useScrollFadeIn";
 
 const Section01: React.FC = () => {
-  return <S.Section>Section01</S.Section>;
+  const firstAnimated = useScrollFadeIn("up", 1, 0);
+  const secondAnimated = useScrollFadeIn("up", 1, 0.1);
+  const thirdAnimated = useScrollFadeIn("up", 1, 0.1);
+  const fourthAnimated = useScrollFadeIn("up", 1, 0.3);
+
+  return (
+    <S.Section>
+      <S.TextWrapper>
+        <S.StarLine {...fourthAnimated}>
+          <img className="star" src="/images/star2.png" alt="star" />
+          <img className="line" src="/images/line.png" alt="line" />
+        </S.StarLine>
+        <S.Span {...firstAnimated}>책 읽는 재미,</S.Span>
+        <S.Span {...secondAnimated}>땅콩스쿨이</S.Span>
+        <S.Span {...thirdAnimated}>만들어줄게요!</S.Span>
+      </S.TextWrapper>
+      <S.MouseAnimation>
+        <img
+          src="https://ddangkongschool.com/82fb322af4738d0bcf0dcf87834209a5.png"
+          alt="move-mouse"
+        />
+      </S.MouseAnimation>
+    </S.Section>
+  );
 };
 
 export default Section01;
--- components/Section01/styled.ts
+++ components/Section01/styled.ts
@@ -1,3 +1,78 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const Section = styled.section``;
+export const Section = styled.section`
+  height: 100vh;
+  width: 100%;
+  background-image: url("https://ddangkongschool.com/8358b32838f4b212cf113ad6c4ea9996.jpg");
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat no-repeat;
+  position: relative;
+  display: flex;
+  justify-content: center;
+`;
+
+export const TextWrapper = styled.div`
+  padding-left: 0px;
+  padding-right: 0px;
+  max-width: 1140px;
+  width: 100%;
+  position: absolute;
+  top: 230px;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+  box-sizing: border-box;
+`;
+
+export const StarLine = styled.div`
+  .star {
+    position: absolute;
+    top: 60px;
+    left: -23px;
+    width: 51.42px;
+  }
+
+  .line {
+    position: absolute;
+    top: 157px;
+    left: 2px;
+    width: 220px;
+  }
+`;
+
+export const Span = styled.span`
+  font-weight: bold;
+  font-size: 60px;
+  line-height: 80px;
+  color: white;
+`;
+
+export const MouseAnimation = styled.div`
+  width: 3.8%;
+  min-width: 48px;
+  position: absolute;
+  animation: motion 0.7s 0s infinite alternate;
+  display: block;
+  bottom: 0px;
+  padding-bottom: 50px;
+  left: 50%;
+
+  @keyframes motion {
+    0% {
+      bottom: 0px;
+    }
+    to {
+      bottom: 100px;
+    }
+  }
+
+  img {
+    width: 72px;
+    height: 72px;
+    bottom: 50px;
+    margin-top: 0px;
+    margin-right: auto;
+  }
+`;
