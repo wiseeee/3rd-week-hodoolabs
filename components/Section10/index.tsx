359ddb3cad079d56fdbcfbb746f58f5289893d1a
wiseeee <yji930714@gmail.com>
2022-02-15 15:34:52 +0900

    feat: section10 완료

--- components/Section10/index.tsx
+++ components/Section10/index.tsx
@@ -1,5 +1,66 @@
-import { Props } from "shared/const";
+import React, {useState, useEffect, useRef} from 'react';
+import * as S from './styled';
 
-const Section09 = ({ name }: Props) => <div>{name}</div>;
+interface Size {
+  width: number;
+}
+const Section10: React.FC = () => {
+  const [size, setSize] = useState<Size>();
+  const resizeHandler = () => {
+    const width = window.innerWidth;
+    setSize({
+      width: width,
+    });
+    if(width > 1200) {
+      document.querySelector('img').src ='/images/coupon1200.png'
+    } else if (width > 1088) {
+      document.querySelector('img').src ='/images/coupon1088.png'
+    } else if (width > 994) {
+      document.querySelector('img').src ='/images/coupon994.png'
+    } else if (width > 900) {
+      document.querySelector('img').src ='/images/coupon900.png'
+    } else if (width > 768) {
+      document.querySelector('img').src ='/images/coupon768.png'
+    } else if (width <= 767.9) {
+      document.querySelector('img').src ='/images/coupon_mobile.png'
+    }
+  };
 
-export default Section09;
+  useEffect(() => {
+    window.onresize = resizeHandler;
+    const width = window.innerWidth;
+    if(width > 1200) {
+      document.querySelector('img').src ='/images/coupon1200.png'
+    } else if (width > 1088) {
+      document.querySelector('img').src ='/images/coupon1088.png'
+    } else if (width > 994) {
+      document.querySelector('img').src ='/images/coupon994.png'
+    } else if (width > 900) {
+      document.querySelector('img').src ='/images/coupon900.png'
+    } else if (width > 768) {
+      document.querySelector('img').src ='/images/coupon768.png'
+    } else if (width <= 767.9) {
+      document.querySelector('img').src ='/images/coupon_mobile.png'
+    }
+  }, []);
+
+  return (
+    <S.Section>
+      <S.SectionTop>지금 다운로드 받으세요!</S.SectionTop>
+      <S.CouponWrap>
+        <img/>
+        <S.CouponLeft>
+          <img src="/images/logo_text_white.png" alt="땅콩스쿨 로고"></img>
+          <S.CouponTitle>책을 보는<br />새로운 방법</S.CouponTitle>
+          <S.CouponSub>지금 가입하시면 <br/>매주 8권의 동화책을 보실 수 있어요!</S.CouponSub>
+        </S.CouponLeft>
+        <S.CouponRight>
+          <S.DownloadCaption>회원가입에 걸리는 시간 단 3초</S.DownloadCaption>
+          <S.ButtonBlack>앱 다운로드 하기</S.ButtonBlack>
+        </S.CouponRight>
+      </S.CouponWrap>
+    </S.Section>
+  )
+};
+
+export default Section10;
---
+++ components/Section10/styled.ts
@@ -0,0 +1,204 @@
+import styled from 'styled-components';
+import { media, theme } from "styles/theme";
+
+export const Section = styled.section`
+  background-color: #F9F9F9;
+  max-width: 1140px;
+  margin: 0 auto;
+  padding: 150px 20px;
+`;
+export const SectionTop = styled.div`
+  font-weight: bold;
+  font-size: 50px;
+  line-height: 70px;
+  text-align: center;
+  word-break: keep-all;
+`;
+export const CouponWrap = styled.div`
+  display: flex;
+  position: relative;
+  width: 1240px;
+  height: 534px;
+  /* background-color: ${theme.color.orange}; */
+  > img {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+  }
+  > div {
+    z-index: 2;
+  }
+  @media (max-width: 1200px) {
+    width: 1088px;
+    height: 534px;
+  }
+  @media (max-width: 1088px) {
+    width: 994px;
+    height: 534px;
+  }
+  @media (max-width: 994px) {
+    width: 900px;
+    height: 570px;
+  }
+  @media (max-width: 900px) {
+    width: 808px;
+    height: 534px;
+    margin-bottom: 80px;
+  }
+  @media (max-width: 768px) {
+    width: 360px;
+    height: 613px;
+    flex-direction: column;
+    margin: 60px auto 116px;
+  }
+`;
+export const CouponLeft = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  box-sizing: border-box;
+  width: 640px;
+  padding-left: 140px;
+  padding-top: 124px;
+  > img {
+    height: 30px;
+  }
+  ${media.mobile} {
+    > img {
+      height: 18px;
+    }
+  }
+  @media (max-width: 1200px) {
+    width: 591px;
+    padding-left: 93px;
+    padding-top: 124px;
+  }
+  @media (max-width: 1088px) {
+    width: 572px;
+    padding-left: 79px;
+    padding-top: 124px;
+  }
+  @media (max-width: 994px) {
+    width: 436px;
+    padding-left: 83px;
+    padding-top: 124px;
+  }
+  @media (max-width: 900px) {
+    width: 407px;
+    padding-left: 66px;
+    padding-top: 124px;
+  }
+  @media (max-width: 768px) {
+    width: 360px;
+    padding-top: 102px;
+    padding-left: 0px;
+  }
+`;
+export const CouponRight = styled.div`
+  box-sizing: border-box;
+  padding-left: 220px;
+  padding-top: 182px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  @media (max-width: 1200px) {
+    padding-left: 140px;
+    padding-top: 182px;
+  }
+  @media (max-width: 1088px) {
+    padding-left: 92px;
+    padding-top: 182px;
+  }
+  @media (max-width: 994px) {
+    padding-left: 103px;
+    padding-top: 200px;
+  }
+  @media (max-width: 900px) {
+    padding-left: 80px;
+    padding-top: 198px;
+  }
+  @media (max-width: 768px) {
+    padding-top: 107px;
+    padding-left: 0;
+  }
+`;
+export const CouponTitle = styled.div`
+  font-size: 50px;
+  line-height: 72px;
+  white-space: pre-wrap;
+  word-break: keep-all;
+  text-align: center;
+  padding-top: 30px;
+  ${media.mobile} {
+    font-size: 36px;
+    line-height: 45px;
+    padding-top: 10px;
+  }
+`;
+export const CouponSub = styled.div`
+  font-size: 20px;
+  line-height: 36px;
+  white-space: pre-wrap;
+  word-break: keep-all;
+  text-align: center;
+  padding-top: 30px;
+  br {
+      display: none;
+    }
+  @media (max-width: 994px) {
+    br {
+      display: block!important;
+    }
+  }
+  ${media.mobile} {
+    font-size: 14px;
+    line-height: 25px;
+    padding-top: 10px;
+  }
+`;
+export const DownloadCaption = styled.div`
+  display: inline-block;
+  text-align: center;
+  font-size: 14px;
+  line-height: 24px;
+  color: #F9F9F9;
+  padding: 10px 15px;
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 50px;
+  margin-bottom: 17px;
+  position:relative;
+  :after {
+    content:'';
+    display: block;
+    position: absolute;
+    bottom: -10px;
+    left: 50%;
+    transform: translate(-50%, 0);
+    width: 0;
+    height: 0;
+    border-style: solid;
+    border-width: 10px 5px 0 5px;
+    border-color: rgba(0, 0, 0, 0.5) transparent transparent transparent;
+  }
+  ${media.mobile} {
+    font-size: 14px;
+    padding: 5px 20px;
+  }
+`;
+export const ButtonBlack = styled.button`
+  display: block;
+  text-align: center;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 32px;
+  color: #F9F9F9;
+  padding: 19px 40px;
+  background:  #333333;;
+  border-radius: 50px;
+  ${media.mobile} {
+    font-size: 14px;
+    border-radius: 10px;
+    padding: 10px 50px;
+  }
+`;
