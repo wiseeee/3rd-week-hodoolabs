4ee28b5a3590a86c542c000481bcf681fa6df586
minbr0ther <minbr0ther@naver.com>
2022-02-15 16:09:14 +0900

    chore: theme에 bgGray추가

--- styles/styled.d.ts
+++ styles/styled.d.ts
@@ -5,6 +5,7 @@ declare module "styled-components" {
     color: {
       orange: "#FFB100";
       black: "#333333";
+      bgGray: "rgb(249, 249, 249)";
     };
     filter: {
       boxShadow: "drop-shadow(10px 10px 10px rgba(0, 0, 0, 0.1))";
